42e58bd403a374520f731f965a4e2c0b8c30aca8
doldesenvolvimento <dol.desenvolvimento@gmail.com>
2022-05-19 11:26:25 -0300

    Update legal.php

--- config/legal.php
+++ config/legal.php
@@ -9,6 +9,6 @@ return [
 
     'success' => 'Operação realizada com sucesso.',
     'error' => 'Falha ao realizar operação. Por favor, tente novamente mais tarde!',
-    'ee' => 'Esta publicação é certificada pelo Diário do Pará e foi publicada na edição digital do jornal. Sua autenticidade pode ser verificada no QrCode ao lado ou através do link: ',
+    'ee' => 'Esta publica&#231;&#227;o &#233; certificada pelo Di&#225;rio do Par&#225; e foi publicada na edi&#231;&#227;o digital do jornal. Sua autenticidade pode ser verificada no QrCode ao lado ou atrav&#233;s do link: ',
     'caminho' => env('CAMINHO'),
 ];
