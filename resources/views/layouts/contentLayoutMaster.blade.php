7bf8cbdf556a797e8b8e3a8425be9e27ab462e5b
doldesenvolvimento <dol.desenvolvimento@gmail.com>
2022-05-27 15:10:35 -0300

    Ajustes das descrições

--- resources/views/layouts/contentLayoutMaster.blade.php
+++ resources/views/layouts/contentLayoutMaster.blade.php
@@ -17,7 +17,7 @@ data-textdirection="{{ env('MIX_CONTENT_DIRECTION') === 'rtl' ? 'rtl' : 'ltr' }}
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <meta name="description" content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
+  <meta name="description" content="Portal para acompanhamento das Publicidades Legais dos clientes do Jornal Diário do Pará">
   <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
   <meta name="author" content="PIXINVENT">
   <title>@yield('title') - Publicação Legal - Diário do Pará</title>
